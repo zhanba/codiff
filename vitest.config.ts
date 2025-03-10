f970199af551937b999fd380b2f2d90f98ed42b6
zhanba <c5e1856@gmail.com>
2025-03-10 22:43:22 +0800

    ci: add test coverage on ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     workspace: ["packages/*"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 });
